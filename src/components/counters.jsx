4030b6fc58c15b2a1001e8e2e08ef639b7b48060
Naveen3129 <naveen.kumar5bc4d@cognizant.com>
2020-04-15 15:55:25 +0530

    initial commit

---
+++ src/components/counters.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import Counter from './counter';
+
+class Counters extends Component {
+    state = { 
+   counters : [
+       {id:1,value :4},
+       {id:2,value :0},
+       {id:3,value :0},
+       {id:4,value :0}
+    ]
+ };
+ handleIncrement = counter => {
+console.log(counter);
+ };
+
+ handleDelete = (counterId) => {
+     const counters =this.state.counters.filter( c=>c.id!==counterId);
+     this.setState({counters})
+ }
+    render() { 
+        return (  
+        <div>
+      {this.state.counters.map(counter => (
+      <Counter key={counter.id} onDelete={this.handleDelete} 
+      onIncrement={this.handleIncrement}
+      counter ={counter}
+      />
+        
+          ))}
+
+
+
+
+        </div>);
+    }
+}
+ 
+export default Counters;
